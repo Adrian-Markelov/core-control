677df8a20ae62805279ca4529da29e74b2212435
Derek Hamersly <derek@coho.org>
2020-02-14 23:15:05 -0500

    added 2d to 3d

---
+++ 2d_to_3d.py
@@ -0,0 +1,9 @@
+#goal: convert 2d to 3d
+
+inches_to_pixels = 100
+height = 10
+
+body_tracking = {"Nose":[],"Neck":[],"RShoulder":[],"RElbow":[],"RWrist":[],
+"LShoulder":[],"LElbow":[],"LWrist":[],"MidHip":[],"RHip":[],"RKnee":[],"RAnkle":[],"LHip":[],"LKnee":[],"LAnkle":[],"REye":[],"LEye":[],"REar":[],"LEar":[],"LBigToe":[],"LSmallToe":[],"LHeel":[],"RBigToe":[],"RSmallToe":[],"RHeel":[],"Background":[]}
+
+
